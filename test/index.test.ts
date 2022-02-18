dac75fe8dfed16159605c518e9d6c5f8891065ae
moritzmhmk <moritzmhmk@gmail.com>
2022-02-18 21:28:51 +0100

    add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,27 @@
+import { readFileSync } from "fs";
 import { parse } from "../src/index";
 import { expect } from "chai";
 
-it("parses a single Integer value", () => {
-  const buffer = new ArrayBuffer(1);
-  const view = new DataView(buffer);
-  view.setInt8(0, 3);
+it("parses a DICOM file", () => {
+  const data = readFileSync(
+    "./test/dicom-files/pydicom-data/data/693_J2KR.dcm"
+  );
+  const dataView = new DataView(new Uint8Array(data).buffer);
+  expect(parse.bind(null, dataView)).to.not.throw();
+});
 
-  expect(parse(view)).to.equal(3);
+it("parses a DICOM file", () => {
+  const data = readFileSync(
+    "./test/dicom-files/pydicom-data/data/bad_sequence.dcm"
+  );
+  const dataView = new DataView(new Uint8Array(data).buffer);
+  expect(parse.bind(null, dataView)).to.not.throw();
+});
+
+it("parses a DICOM file", () => {
+  const data = readFileSync(
+    "./test/dicom-files/pydicom-data/data/emri_small_big_endian.dcm"
+  );
+  const dataView = new DataView(new Uint8Array(data).buffer);
+  expect(parse.bind(null, dataView)).to.not.throw();
 });
