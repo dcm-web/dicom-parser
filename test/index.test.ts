6e9ea7a40dfcdf4571e088f86491cbec9b55bc9f
moritzmhmk <moritzmhmk@gmail.com>
2022-02-20 00:40:27 +0100

    test for extra/missing tags

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,30 +1,26 @@
-import { readFileSync } from "fs";
+import * as fs from "fs";
 import * as parser from "../src/parser";
 import { expect } from "chai";
 
-it("parses all DataElements of a DICOM file", () => {
-  const data = readFileSync(
-    "./test/dicom-files/pydicom-data/data/693_J2KR.dcm"
-  );
-  const dataView = new DataView(new Uint8Array(data).buffer);
-  const dataSet = parser.parse(dataView);
-  expect(Object.keys(dataSet).length).to.equal(86);
-});
+const files = fs.readdirSync("./test/dicom-files/pydicom/"); // ["693_J2KR.dcm", "bad_sequence.dcm", "emri_small_big_endian.dcm"];
 
-it("parses all DataElements of a DICOM file", () => {
-  const data = readFileSync(
-    "./test/dicom-files/pydicom-data/data/bad_sequence.dcm"
-  );
-  const dataView = new DataView(new Uint8Array(data).buffer);
-  const dataSet = parser.parse(dataView);
-  expect(Object.keys(dataSet).length).to.equal(87);
-});
+files.forEach((file) => {
+  if (fs.lstatSync("./test/dicom-files/pydicom/" + file).isDirectory()) return;
 
-it("parses all DataElements of a DICOM file", () => {
-  const data = readFileSync(
-    "./test/dicom-files/pydicom-data/data/emri_small_big_endian.dcm"
-  );
-  const dataView = new DataView(new Uint8Array(data).buffer);
-  const dataSet = parser.parse(dataView);
-  expect(Object.keys(dataSet).length).to.equal(139);
+  it(`parses all DataElements of DICOM file "${file}"`, () => {
+    const expectedTags = JSON.parse(
+      fs.readFileSync(`./test/dicom-tags/pydicom/${file}.json`, "utf8")
+    ) as string[];
+
+    const data = fs.readFileSync(`./test/dicom-files/pydicom/${file}`);
+    const dataView = new DataView(new Uint8Array(data).buffer);
+    const dataSet = parser.parse(dataView);
+    const tags = Object.keys(dataSet);
+
+    const extraTags = tags.filter((tag) => !expectedTags.includes(tag));
+    const missingTags = expectedTags.filter((tag) => !tags.includes(tag));
+
+    expect(extraTags, "extra tags").to.eql([]);
+    expect(missingTags, "missing tags").to.eql([]);
+  });
 });
