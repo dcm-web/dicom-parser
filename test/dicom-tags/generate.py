be201e55a492005ec6c8bb7fad4bb531a9041886
moritzmhmk <moritzmhmk@gmail.com>
2022-02-20 00:39:53 +0100

    add json files of the expected tags for the dicom files

---
+++ test/dicom-tags/generate.py
@@ -0,0 +1,21 @@
+import json
+import os
+import pydicom
+
+setname = 'pydicom'
+in_dir = os.path.join(os.path.dirname(__file__), '..', 'dicom-files', setname)
+for filename in os.listdir(in_dir):
+    in_file = os.path.join(in_dir, filename)
+    if not os.path.isfile(in_file):
+        continue
+    print(f"generating json of tags for file {filename}")
+    ds = pydicom.read_file(in_file, force=True)
+
+    def tag_string(element):
+        return str(element.tag).replace(" ", "")
+
+    tags = [tag_string(de) for de in ds.file_meta] + [tag_string(de) for de in ds]
+
+    out_dir = os.path.join(os.path.dirname(__file__), setname)
+    with open(os.path.join(out_dir, f'{filename}.json'), 'w') as outfile:
+        outfile.write(json.dumps(tags))
