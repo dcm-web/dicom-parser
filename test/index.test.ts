41659057570bd6dae9b2576d82b56bce887e4e50
moritzmhmk <moritzmhmk@gmail.com>
2022-02-19 21:00:49 +0100

    change tests to count DataElements found

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,26 +2,29 @@ import { readFileSync } from "fs";
 import * as parser from "../src/parser";
 import { expect } from "chai";
 
-it("parses a DICOM file", () => {
+it("parses all DataElements of a DICOM file", () => {
   const data = readFileSync(
     "./test/dicom-files/pydicom-data/data/693_J2KR.dcm"
   );
   const dataView = new DataView(new Uint8Array(data).buffer);
-  expect(parser.parse.bind(null, dataView)).to.not.throw();
+  const dataSet = parser.parse(dataView);
+  expect(Object.keys(dataSet).length).to.equal(86);
 });
 
-it("parses a DICOM file", () => {
+it("parses all DataElements of a DICOM file", () => {
   const data = readFileSync(
     "./test/dicom-files/pydicom-data/data/bad_sequence.dcm"
   );
   const dataView = new DataView(new Uint8Array(data).buffer);
-  expect(parser.parse.bind(null, dataView)).to.not.throw();
+  const dataSet = parser.parse(dataView);
+  expect(Object.keys(dataSet).length).to.equal(87);
 });
 
-it("parses a DICOM file", () => {
+it("parses all DataElements of a DICOM file", () => {
   const data = readFileSync(
     "./test/dicom-files/pydicom-data/data/emri_small_big_endian.dcm"
   );
   const dataView = new DataView(new Uint8Array(data).buffer);
-  expect(parser.parse.bind(null, dataView)).to.not.throw();
+  const dataSet = parser.parse(dataView);
+  expect(Object.keys(dataSet).length).to.equal(139);
 });
