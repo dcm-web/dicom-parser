2fbe787b26f0214cbe8a8caf7cda32d071022f38
moritzmhmk <moritzmhmk@gmail.com>
2022-02-23 22:54:32 +0100

    test parsed pixel data

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,7 @@
+import { createHash } from "crypto";
 import * as fs from "fs";
 import * as parser from "../src/parser";
+import * as utils from "../src/parser/utils";
 import { expect } from "chai";
 
 const dicomFilesDir = "./test/dicom-files";
@@ -9,6 +11,7 @@ const fileSets = ["pydicom"] as const;
 const excludeFiles: Record<typeof fileSets[number], string[]> = {
   pydicom: [
     "OT-PAL-8-face.dcm", // file without header
+    "emri_small_jpeg_2k_lossless_too_short.dcm", // Pixel Data has no Delimitation Tag
   ],
 };
 
@@ -26,6 +29,7 @@ function readDataView(fileSet: string, file: string) {
 }
 type FileData = {
   tags: string[];
+  pixelDataHash: string;
 };
 function readFileData(fileSet: string, file: string): FileData {
   return JSON.parse(
@@ -58,4 +62,36 @@ it("should not parse file without header", () => {
   expect(() => parser.parse(dataView)).to.throw(
     "Invalid DICOM file - prefix not found."
   );
+});
+
+it("should not parse file with pixel data that lacks a delimitation tag", () => {
+  const dataView = readDataView(
+    "pydicom",
+    "emri_small_jpeg_2k_lossless_too_short.dcm"
+  );
+  expect(() => parser.parse(dataView)).to.throw(
+    "Reached end of file searching for a Sequence Delimitation Tag"
+  );
+});
+
+fileSets.forEach((fileSet) => {
+  fileSetFiles(fileSet)
+    .filter((file) => !excludeFiles[fileSet]?.includes(file))
+    .forEach((file) => {
+      it(`should parse pixel data value of DICOM file "${fileSet}/${file}"`, () => {
+        const dataView = readDataView(fileSet, file);
+        const expectedPixelDataHash = readFileData(fileSet, file).pixelDataHash;
+        const { dataSet } = parser.parse(dataView);
+        const pixelDataElement = dataSet["(7fe0,0010)"];
+
+        const pixelDataView = utils.dataViewAtLocation(
+          dataView,
+          pixelDataElement.value
+        );
+        const pixelDataHash = createHash("sha1")
+          .update(pixelDataView)
+          .digest("hex");
+        expect(pixelDataHash).to.equal(expectedPixelDataHash);
+      });
+    });
 });
